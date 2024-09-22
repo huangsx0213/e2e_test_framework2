cf5acb2a9b3875f4cc157a820ec2060ca92a9654
huangsx0213 <huangsx0213@163.com>
2024-09-22 20:59:13 +0800

    version 1.1.0

--- src/test/java/api/TestSuite.java
+++ src/test/java/api/TestSuite.java
@@ -6,11 +6,12 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("/features")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "api.StepDefinitions")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty,html:target/report.html")
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@get-data")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,html:target/cucumber.html")
 public class TestSuite {
 }
